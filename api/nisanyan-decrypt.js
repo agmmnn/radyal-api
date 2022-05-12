6f696edc023729ea7a12bc81b2089325d81d2994
Gökçe <agmerdun@hotmail.com>
2022-05-12 12:27:54 +0300

    Update nisanyan-decrypt.js

--- api/nisanyan-decrypt.js
+++ api/nisanyan-decrypt.js
@@ -2,8 +2,6 @@ const axios = require("axios").default;
 const AES = require("crypto-js/aes");
 const Utf8 = require("crypto-js/enc-utf8");
 
-// https://radyal-api-nodejs.vercel.app/api/nisdec?word=kaynak
-
 module.exports = (req, res) => {
   if (req.query.word) {
     let resp;
@@ -13,7 +11,6 @@ module.exports = (req, res) => {
       .get(`https://www.nisanyansozluk.com/api/words/${word}?session=${sId}`)
       .then((response) => (resp = response.data))
       .then(() => res.json(JSON.parse(decrypt(resp))))
-      // .then(() => output(JSON.parse(decrypt(resp))))
       .catch((error) => console.log("error", error));
   } else if (req.query.text) {
     const text = req.query.text;
@@ -27,18 +24,4 @@ function decrypt(txt) {
   let key = '#d*jcsrS!U+16eHoZGu"_}X{m<Q49W';
   let str = AES.decrypt(txt, key).toString(Utf8);
   return str;
-}
-
-function output(str) {
-  if (str.isUnsuccessful === false) {
-    console.log(str);
-  } else {
-    for (let i = 0; i < str.fiveBefore.length; i++) {
-      console.log(str.fiveBefore[i].name);
-    }
-    console.log(str.words[0].name);
-    for (let i = 0; i < str.fiveAfter.length; i++) {
-      console.log(str.fiveAfter[i].name);
-    }
-  }
 }
