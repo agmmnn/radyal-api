c4823388614d3ae4ddf5f71e1aff4dc451e582f1
Gökçe <agmerdun@hotmail.com>
2022-05-15 01:47:44 +0300

    add: esanlam endpoint

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Provides some useful API endpoints.
 ### [nisanyan-decrypt](https://radyal-api.vercel.app/api/nisanyan-decrypt)
 
 Decode the result returned from Nisanyansozluk.com's API, [example url](https://radyal-api.vercel.app/api/nisanyan-decrypt?word=sanat).
+
+### [esanlam](https://radyal-api.vercel.app/api/esanlam)
+
+Turkish thesaurus returns synonyms and antonyms for the given word, [example url](https://radyal-api.vercel.app/api/esanlam?word=durmak).
---
+++ api/esanlam.js
@@ -0,0 +1,38 @@
+module.exports = async (req, res) => {
+  const word = req.query.word;
+  if (word) {
+    const db = await connectToDatabase();
+    const coll = await db.collection("thesaurus_tr");
+    await coll.findOne({ word: word }, (err, result) => {
+      if (err) throw err;
+      delete result._id;
+      res.status(200).json(result);
+    });
+  } else {
+    res.status(200).json({ error: "no word given" });
+  }
+};
+
+const MongoClient = require("mongodb").MongoClient;
+
+let cachedDb = null;
+
+const connectToDatabase = async () => {
+  if (cachedDb) {
+    console.log("Use existing connection");
+    return Promise.resolve(cachedDb);
+  } else {
+    return MongoClient.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+    })
+      .then((client) => {
+        let db = client.db("thesaurus_tr");
+        console.log("New Database connection");
+        cachedDb = db;
+        return cachedDb;
+      })
+      .catch((error) => {
+        console.log("MongoDB connection error: " + error);
+      });
+  }
+};
