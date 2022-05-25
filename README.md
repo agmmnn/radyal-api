03968b5c160c9b31dc0cc94b09d91499cf80c9b6
Gökçe <agmerdun@hotmail.com>
2022-05-26 01:47:19 +0300

    add: lugat endpoint

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Decode the result returned from Nisanyansozluk.com's API, [example url](https://
 ### [esanlam](https://radyal-api.vercel.app/api/esanlam)
 
 Turkish thesaurus returns synonyms and antonyms for the given word, [example url](https://radyal-api.vercel.app/api/esanlam?word=durmak).
+
+### [lugat](https://radyal-api.vercel.app/api/lugat)
+
+Ottoman Turkish to Modern Turkish dictionary, [example url](https://radyal-api.vercel.app/api/lugat?word=divane) [\*](https://radyal-api.vercel.app/api/lugat?word=dîvâne) [\*](https://radyal-api.vercel.app/api/lugat?word=د۪يوَانَه).
---
+++ api/lugat.js
@@ -0,0 +1,45 @@
+module.exports = async (req, res) => {
+  const word = req.query.word;
+  if (word) {
+    const db = await connectToDatabase();
+    const coll = await db.collection("lugat");
+    await coll.findOne(
+      { $or: [{ word: word }, { other_forms: word }, { ar_script: word }] },
+      (err, result) => {
+        if (err) throw err;
+        if (result) {
+          delete result._id;
+          res.status(200).json(result);
+        } else {
+          res.status(200).json({ msg: "not found" });
+        }
+      }
+    );
+  } else {
+    res.status(200).json({ error: "no word given" });
+  }
+};
+
+const MongoClient = require("mongodb").MongoClient;
+
+let cachedDb = null;
+
+const connectToDatabase = async () => {
+  if (cachedDb) {
+    console.log("Use existing connection");
+    return Promise.resolve(cachedDb);
+  } else {
+    return MongoClient.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+    })
+      .then((client) => {
+        let db = client.db("lugat");
+        console.log("New Database connection");
+        cachedDb = db;
+        return cachedDb;
+      })
+      .catch((error) => {
+        console.log("MongoDB connection error: " + error);
+      });
+  }
+};
