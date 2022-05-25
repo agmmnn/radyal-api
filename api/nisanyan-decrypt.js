758d7db29ce08d45fb3e103643b39593ed8b7ee3
Gökçe <agmerdun@hotmail.com>
2022-05-25 14:00:18 +0300

    Update nisanyan-decrypt.js

--- api/nisanyan-decrypt.js
+++ api/nisanyan-decrypt.js
@@ -2,13 +2,18 @@ const axios = require("axios").default;
 const AES = require("crypto-js/aes");
 const Utf8 = require("crypto-js/enc-utf8");
 
+const SECRET = '#d*jcsrS!U+16eHoZGu"_}X{m<Q49W';
+const SESSION_ID = "604920fa-c61e-4107-bf5d-2c6e87791805";
+
 module.exports = (req, res) => {
-  if (req.query.word) {
+  let word = req.query.word;
+  if (word) {
+    word = encodeURI(req.query.word);
     let resp;
-    const sId = "604920fa-c61e-4107-bf5d-2c6e87791805";
-    const word = encodeURI(req.query.word);
     axios
-      .get(`https://www.nisanyansozluk.com/api/words/${word}?session=${sId}`)
+      .get(
+        `https://www.nisanyansozluk.com/api/words/${word}?session=${SESSION_ID}`
+      )
       .then((response) => (resp = response.data))
       .then(() => res.json(JSON.parse(decrypt(resp))))
       .catch((error) => console.log("error", error));
@@ -21,7 +26,5 @@ module.exports = (req, res) => {
 };
 
 function decrypt(txt) {
-  let key = '#d*jcsrS!U+16eHoZGu"_}X{m<Q49W';
-  let str = AES.decrypt(txt, key).toString(Utf8);
-  return str;
+  return AES.decrypt(txt, SECRET).toString(Utf8);
 }
