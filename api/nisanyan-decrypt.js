1ff16bb6dc76f2022aa81fb3d03eb4984fef481f
Gökçe <agmmnn@gmail.com>
2022-11-24 00:40:56 +0300

    Add: url request

--- api/nisanyan-decrypt.js
+++ api/nisanyan-decrypt.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const CryptoJS = require("crypto-js");
 const AES = require("crypto-js/aes");
 const Utf8 = require("crypto-js/enc-utf8");
 
@@ -7,25 +6,41 @@ const SECRET = 'v03_ggh"fgjsa"{gffszXg}31S!AdX';
 const SECRET2 = 'fdfd_{fdgfds4@"dfg"}VbvxXSfd4Z';
 
 module.exports = (req, res) => {
-  // POST request, encrypted body
-  // https://www.nisanyansozluk.com/api/words/sanat?session=1
   if (req.method === "POST") {
+    // POST http://localhost:3000/api/nisanyan-decrypt <-[encrypted body]
+    // https://www.nisanyansozluk.com/api/words/sanat?session=1
     const { body } = req;
     console.log(body);
     res.json(JSON.parse(decrypt(body.toString(), SECRET2)));
   }
 
+  const get_data = async (url, secret) => {
+    return axios
+      .get(url)
+      .then((response) => JSON.parse(decrypt(response.data, secret)))
+      .catch((error) => console.log(error));
+  };
+
   let word = req.query.word;
   if (word) {
+    // word request
+    // http://localhost:3000/api/nisanyan-decrypt?word=sanat
     word = encodeURI(req.query.word);
-    axios
-      .get(`https://www.nisanyansozluk.com/api/words/${word}?session=1`)
-      .then((response) => res.json(JSON.parse(decrypt(response.data, SECRET2))))
-      .catch((error) => {
-        console.log("error", error);
-        res.json({ error: error.toString() });
-      });
+    const url = `https://www.nisanyansozluk.com/api/words/${word}?session=1`;
+    get_data(url, SECRET2).then((resp) => res.json(resp));
+  } else if (req.query.url) {
+    // url request
+    // http://localhost:3000/api/nisanyan-decrypt?url=<encoded_url>
+    const url = decodeURIComponent(
+      req.url.split("/api/nisanyan-decrypt?url=")[1]
+    );
+    console.log(url);
+    get_data(url, SECRET2).then((resp) => {
+      res.json(resp);
+    });
   } else {
+    // home request
+    // http://localhost:3000/api/nisanyan-decrypt
     axios
       .get(`https://www.nisanyansozluk.com/api/home`)
       .then((response) => res.json(response.data))
--- api/nisanyanadlar-decrypt.js
+++ api/nisanyanadlar-decrypt.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const AES = require("crypto-js/aes");
 const Utf8 = require("crypto-js/enc-utf8");
-const Latin1 = require("crypto-js/enc-latin1");
 
 const SECRET_app = 'RFfss@{4_232D"vgsS"5}_@455dzZh';
 const SECRET_name = "fgdRT%{@259cvzzE_sd!}dfcj_vxXq";
@@ -14,23 +13,50 @@ module.exports = (req, res) => {
     res.json(JSON.parse(decrypt(body, SECRET_name)));
   }
 
+  const get_data = async (url, secret) => {
+    return axios
+      .get(url)
+      .then((response) => JSON.parse(decrypt(response.data, secret)))
+      .catch((error) => console.log(error));
+  };
+
   let name = req.query.name;
   if (name) {
+    // name request
+    // http://localhost:3000/api/nisanyanadlar-decrypt?name=Tigin
     name = encodeURI(req.query.name);
     url = `https://www.nisanyanadlar.com/api/names/${name}?gender=all&session=1`;
-    axios
-      .get(url)
-      .then((response) =>
-        res.json(JSON.parse(decrypt(response.data, SECRET_name)))
-      )
-      .catch((error) => console.log("error", error));
+    get_data(url, SECRET_name).then((resp) => {
+      res.json(resp);
+    });
+  } else if (req.query.url) {
+    // url request
+    // http://localhost:3000/api/nisanyanadlar-decrypt?url=<encoded_url>
+    const url = decodeURIComponent(
+      req.url.split("/api/nisanyanadlar-decrypt?url=")[1]
+    );
+    console.log(url);
+
+    try {
+      get_data(url, SECRET_name).then((resp) => {
+        res.json(resp);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    try {
+      get_data(url, SECRET_app).then((resp) => {
+        res.json(resp);
+      });
+    } catch (err) {
+      console.log(err);
+    }
   } else {
-    axios
-      .get("https://www.nisanyanadlar.com/api/home")
-      .then((response) =>
-        res.json(JSON.parse(decrypt(response.data, SECRET_app)))
-      )
-      .catch((error) => console.log("error", error));
+    // home request
+    // http://localhost:3000/api/nisanyanadlar-decrypt
+    get_data("https://www.nisanyanadlar.com/api/home", SECRET_app).then(
+      (resp) => res.json(resp)
+    );
   }
 
   function decrypt(txt, secret) {
