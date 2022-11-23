66f0e6eb17e2881e998f574d2288aa9998ebdfbc
Gökçe <agmmnn@gmail.com>
2022-11-23 22:42:07 +0300

    Update: Encryption Secrets, Add: home

--- api/nisanyan-decrypt.js
+++ api/nisanyan-decrypt.js
@@ -1,32 +1,38 @@
 const axios = require("axios");
+const CryptoJS = require("crypto-js");
 const AES = require("crypto-js/aes");
 const Utf8 = require("crypto-js/enc-utf8");
 
-const SECRET = 'dfa{st5W@#!%&!"dcbhJDS%7022Fs5';
+const SECRET = 'v03_ggh"fgjsa"{gffszXg}31S!AdX';
+const SECRET2 = 'fdfd_{fdgfds4@"dfg"}VbvxXSfd4Z';
 
 module.exports = (req, res) => {
+  // POST request, encrypted body
+  // https://www.nisanyansozluk.com/api/words/sanat?session=1
   if (req.method === "POST") {
     const { body } = req;
-    res.json(JSON.parse(decrypt(body)));
+    console.log(body);
+    res.json(JSON.parse(decrypt(body.toString(), SECRET2)));
   }
 
   let word = req.query.word;
   if (word) {
     word = encodeURI(req.query.word);
     axios
-      .get(
-        `https://www.nisanyansozluk.com/api/words/${word}?session=9d78061f-3b58-432b-b8ec-d553b878d622`
-      )
-      .then((response) => res.json(JSON.parse(decrypt(response.data))))
-      .catch((error) => console.log("error", error));
+      .get(`https://www.nisanyansozluk.com/api/words/${word}?session=1`)
+      .then((response) => res.json(JSON.parse(decrypt(response.data, SECRET2))))
+      .catch((error) => {
+        console.log("error", error);
+        res.json({ error: error.toString() });
+      });
   } else {
     axios
       .get(`https://www.nisanyansozluk.com/api/home`)
       .then((response) => res.json(response.data))
       .catch((error) => console.log("error", error));
   }
-};
 
-function decrypt(txt) {
-  return AES.decrypt(txt, SECRET).toString(Utf8);
-}
+  function decrypt(txt, secret) {
+    return AES.decrypt(txt, secret).toString(Utf8);
+  }
+};
