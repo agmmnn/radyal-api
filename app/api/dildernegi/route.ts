d50edc0fa6b6df76b2b41a7ae9bc815cc060af9e
Gökçe Merdun <agmmnn@gmail.com>
2023-04-15 20:30:21 +0300

    workflow

--- app/api/dildernegi/route.ts
+++ app/api/dildernegi/route.ts
@@ -1,19 +1,23 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/app/helpers/connectToDB";
 
-export async function GET(req: Request, res: Response) {
+export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const word = searchParams.get("word")?.toLowerCase();
+  const word = searchParams.get("word");
 
   if (word) {
     const db = await connectToDatabase("dildernegi");
     const coll = await db.collection("dildernegi");
     try {
       const result = await coll.findOne({ word: word });
-      delete result._id;
-      return NextResponse.json(result);
-    } catch {
-      return NextResponse.json({ error: "error" });
+      if (result) {
+        delete result._id;
+        return NextResponse.json(result);
+      } else {
+        return NextResponse.json({ error: "not found" });
+      }
+    } catch (error) {
+      return NextResponse.json({ error: String(error) });
     }
   } else {
     return NextResponse.json({ error: "no word given" });
